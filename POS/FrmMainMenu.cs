e7516b72e047bcabc4135d1fb7ca8956a60e1abd
Angel <angel4441916@gmail.com>
2019-05-19 14:36:25 -0400

    new menu

--- POS/FrmMainMenu.cs
+++ POS/FrmMainMenu.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using POS.FrmsCategoria;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,81 +18,80 @@ namespace POS
             InitializeComponent();
         }
 
-        private void btnSales_Click(object sender, EventArgs e)
-        {
-            FrmPrincipal frmSales = new FrmPrincipal();
-            frmSales.Show();
-            Close();
-        }
-
-        private void btnProducts_Click(object sender, EventArgs e)
-        {
-            FrmProducts frmProducts = new FrmProducts();
-            frmProducts.Show();
-            Close();
-        }
-
-        private void btnReport_Click(object sender, EventArgs e)
-        {
-            FrmReport frmReport = new FrmReport();
-            frmReport.Show();
-            Close();
-        }
-
-        private void btnInvoice_Click(object sender, EventArgs e)
-        {
-            FrmInvoice frmInvoice = new FrmInvoice();
-            frmInvoice.Show();
-            Close();
-        }
-
-        private void btnPurchases_Click(object sender, EventArgs e)
-        {
-            FrmPurchases frmPurchases = new FrmPurchases();
-            frmPurchases.Show();
-            Close();
-        }
-
-        private void btnCompany_Click(object sender, EventArgs e)
-        {
-            FrmCompany frmCompany = new FrmCompany();
-            frmCompany.Show();
-            Close();
-        }
-
-        private void btnProvider_Click(object sender, EventArgs e)
-        {
-            FrmProvider frmProvider = new FrmProvider();
-            frmProvider.Show();
-            Close();
-        }
-
-        private void btnStock_Click(object sender, EventArgs e)
-        {
-            FrmStock frmStock = new FrmStock();
-            frmStock.Show();
-            Close();
-        }
-
-        private void btnStoreConfiguration_Click(object sender, EventArgs e)
-        {
-            FrmStoreConfiguration frmStoreConfiguration = new FrmStoreConfiguration();
-            frmStoreConfiguration.Show();
-            Close();
-        }
-
-        private void btnExit_Click(object sender, EventArgs e)
+        private void btnExit_Click_1(object sender, EventArgs e)
         {
             FrmInicioSesion frmInicioSesion = new FrmInicioSesion();
             frmInicioSesion.Show();
             Close();
         }
 
-        private void btnEmployee_Click(object sender, EventArgs e)
+        private void btnSales_Click_1(object sender, EventArgs e)
         {
-            FrmEmployee frmEmployee = new FrmEmployee();
-            frmEmployee.Show();
+            FrmPrincipal frmSales = new FrmPrincipal();
+            frmSales.Show();
             Close();
         }
-    }
+
+        private void btnProducts_Click_1(object sender, EventArgs e)
+        {
+            FrmProducts frmProducts = new FrmProducts();
+            frmProducts.Show();
+            Close();
+        }
+
+        private void btnReport_Click_1(object sender, EventArgs e)
+        {
+            FrmReport frmReport = new FrmReport();
+            frmReport.Show();
+            Close();
+        }
+
+        private void btnInvoice_Click_1(object sender, EventArgs e)
+        {
+            FrmInvoice frmInvoice = new FrmInvoice();
+            frmInvoice.Show();
+            Close();
+        }
+
+        private void btnPurchases_Click_1(object sender, EventArgs e)
+        {
+            FrmPurchases frmPurchases = new FrmPurchases();
+            frmPurchases.Show();
+            Close();
+        }
+
+        private void btnCompany_Click_1(object sender, EventArgs e)
+        {
+            FrmCompany frmCompany = new FrmCompany();
+            frmCompany.Show();
+            Close();
+        }
+
+        private void btnProvider_Click_1(object sender, EventArgs e)
+        {
+            FrmProvider frmProvider = new FrmProvider();
+            frmProvider.Show();
+            Close();
+        }
+
+        private void btnStock_Click_1(object sender, EventArgs e)
+        {
+            FrmStock frmStock = new FrmStock();
+            frmStock.Show();
+            Close();
+        }
+
+        private void btnStoreConfiguration_Click_1(object sender, EventArgs e)
+        {
+            FrmStoreConfiguration frmStoreConfiguration = new FrmStoreConfiguration();
+            frmStoreConfiguration.Show();
+            Close();
+        }
+
+		private void button11_Click(object sender, EventArgs e)
+		{
+			FrmCategoryMainMenu category = new FrmCategoryMainMenu();
+			category.Show();
+		}
+	}
 }
